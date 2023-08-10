9f2dd0dc94ea4cae9b2ce52a47126c214185841a
Israel_Victor <israel-victor@bol.com.br>
2023-08-10 18:44:37 -0300

    Aula 7 - JSX 1

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
 
-const App = () => {
-  return <h1>Reacts</h1>
-}
+const titulo = <h1>Esse é um Titulo</h1>;
 
-export default App
+const App = () => {
+  const nome = 'Juliano';
+  const ativo = false;
+
+  function mostrarNome(sobrenome) {
+    return 'Michel ' + sobrenome;
+  }
+
+  const carro = {
+    marca: 'ford',
+    rodas: 'quatro',
+  };
+
+  const estiloP = {
+    color: 'gray',
+    fontWeight: '700',
+    fontSize: '40px',
+  };
+
+  return (
+    <>
+      {titulo}
+      <label htmlFor="nome">Nome</label>
+      <input
+        className={ativo ? 'ativo' : 'inativo'}
+        type="text"
+        id="nome"
+        placeholder={nome}
+      />
+
+      <p style={estiloP}>{false ? 'Isso é verdadeiro' : 'Isso é falso'}</p>
+      <br />
+      <p>{mostrarNome('Thenorio')}</p>
+      <br />
+
+      <p>{new Date().getFullYear()}</p>
+      <br />
+
+      <p>
+        {carro.marca}, {carro.rodas}
+      </p>
+    </>
+  );
+};
+
+export default App;
